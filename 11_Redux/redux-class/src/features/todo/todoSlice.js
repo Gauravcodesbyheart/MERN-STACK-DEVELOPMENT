21473eff4c2c790c94ff5bca4d0ebede4b301a3f
gauravcodesbyheart <gauravprofessional786@gmail.com>
2026-02-12 01:01:35 +0530

    make the all possible changes

---
+++ 11_Redux/redux-class/src/features/todo/todoSlice.js
@@ -0,0 +1,36 @@
+//reducers
+
+import {createSlice, nanoid} from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{id: "abc", task: "demo-task", isDone: false}],
+
+}
+
+export const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducer:{
+        addTodo:(state,action)=>{
+            const newTodo = {
+                id: nanoid(),
+                task: action.payload,
+                isDone: false,
+            };
+            state.todos.push(newTodo);  // very different from normal react where we needed to firstly destructure the data then push 
+        },
+        deleteTodo: (state, action)=>{
+            state.todos = state.todos.filter((todo) => todo.id !=action.payload)
+        },
+        marksasDone: (state, action) =>{
+            state.todos = state.todos.map((todo)=>{
+                if (todo.id===action.payload){
+                    todo.isDone = true;
+                }
+            })
+        }
+    }
+});
+
+export const {addTodo, deleteTodo, markAsDone} = todoSlice.actions;
+export default  todoSlice.reducer;
